df31d420db0cf5990e7d63d4ab4bb735d317b2e9
Guilherme Delazeri <guilhermedelazeri@MacBook-Pro-de-Guilherme.local>
2024-04-03 23:16:55 -0300

    feat: add typography component

---
+++ UseOriginMobileDesignSystem/components/Typography/Typography.stories.tsx
@@ -0,0 +1,91 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Typography } from "./Typography";
+
+const meta = {
+  title: "Typography",
+  component: Typography,
+  args: {
+    children: "Typography",
+    variant: 'h1'
+  },
+} satisfies Meta<typeof Typography>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const h1: Story = {
+  args: {
+    children: "Headline 1",
+    variant: 'h1'
+  }
+};
+
+export const h2: Story = {
+  args: {
+    children: "Headline 2",
+    variant: 'h2',
+    color: '#ff0000'
+  }
+};
+
+export const h3: Story = {
+  args: {
+    children: "Headline 3",
+    variant: 'h3',
+    color: '#00ff00'
+  }
+};
+
+export const h4: Story = {
+  args: {
+    children: "Headline 4",
+    variant: 'h4',
+    color: '#0000ff'
+  }
+};
+
+export const h5: Story = {
+  args: {
+    children: "Headline 5",
+    variant: 'h5',
+  }
+};
+
+export const h6: Story = {
+  args: {
+    children: "Headline 6",
+    variant: 'h6',
+    color: '#ff00ff'
+  }
+};
+
+export const subtitle1: Story = {
+  args: {
+    children: "Subtitle 1",
+    variant: 'subtitle1',
+  }
+};
+
+export const subtitle2: Story = {
+  args: {
+    children: "Subtitle 2",
+    variant: 'subtitle2',
+  }
+};
+
+export const body1: Story = {
+  args: {
+    children: "Body 1",
+    variant: 'body1',
+    color: '#000fff'
+  }
+};
+
+export const body2: Story = {
+  args: {
+    children: "Body 2",
+    variant: 'body2',
+    color: '#ff0000'
+  }
+};
---
+++ UseOriginMobileDesignSystem/components/Typography/Typography.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Text } from 'react-native-paper';
+import { TypographyProps } from "./types";
+import { StyleSheet } from "react-native";
+
+export const Typography = ({
+  children,
+  variant = 'h1',
+  color = '#333333',
+}: TypographyProps) => {
+  return (
+    <Text style={{ ...styles[variant], color }}>
+      {children}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  h1: {
+    fontSize: 96,
+    fontWeight: '300',
+    letterSpacing: -1.5,
+  },
+  h2: {
+    fontSize: 60,
+    fontWeight: '300',
+    letterSpacing: -0.5,
+  },
+  h3: {
+    fontSize: 48,
+    fontWeight: '400',
+  },
+  h4: {
+    fontSize: 34,
+    fontWeight: '400',
+    letterSpacing: 0.25,
+  },
+  h5: {
+    fontSize: 24,
+    fontWeight: '400',
+  },
+  h6: {
+    fontSize: 20,
+    fontWeight: '500',
+    letterSpacing: 0.15,
+  },
+  subtitle1: {
+    fontSize: 16,
+    fontWeight: '400',
+    letterSpacing: 0.15,
+  },
+  subtitle2: {
+    fontSize: 14,
+    fontWeight: '500',
+    letterSpacing: 0.1,
+  },
+  body1: {
+    fontSize: 16,
+    fontWeight: '400',
+    letterSpacing: 0.5,
+  },
+  body2: {
+    fontSize: 14,
+    fontWeight: '400',
+    letterSpacing: 0.25,
+  },
+})
